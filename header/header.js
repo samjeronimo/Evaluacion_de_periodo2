c15d47672b20c45443fd694974c7b53352b8db4d
jerónimo <sajeronimo@scl.edu.gt>
2024-07-11 10:33:46 -0600

    header terminado

---
+++ header/header.js
@@ -0,0 +1,10 @@
+let li_header = document.querySelector(".header");
+
+li_header.innerHTML = `
+    <header>
+        <h3>Sajeronimo@</h3>
+        <div class="cuadrito">
+            <h2>github</h2>
+        </div>
+    </header>
+`;
