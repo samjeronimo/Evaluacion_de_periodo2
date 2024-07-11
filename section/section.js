dcfd94dbfcc93df5c083d0beee8c8f011e273506
jerónimo <sajeronimo@scl.edu.gt>
2024-07-11 11:04:31 -0600

    section terminada

---
+++ section/section.js
@@ -0,0 +1,13 @@
+let li_section = document.querySelector(".section")
+
+li_section.innerHTML = `
+        <div class="cuadros">
+            <div class="minicuadros"></div>
+        </div>
+        <div class="cuadros">
+            <div class="minicuadros"></div>
+        </div>
+        <div class="cuadros">
+            <div class="minicuadros"></div>
+        </div>
+`;
